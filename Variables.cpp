e0177c95b76eaa52d7c64015600dabf2b24cf54e
sgnfxn <sgnfxns@gmail.com>
2026-04-16 20:02:37 +0100

    Variables

---
+++ Variables.cpp
@@ -0,0 +1,13 @@
+// Variables and constants
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+    // Defining constant
+    int constexpr hoursInaday = 24;
+
+    // Printing value
+    cout << hoursInaday;
+  	return 0;
+}
